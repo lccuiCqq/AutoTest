8c65d4bd4ba7c0eed12dfc799bf42f94cac4e6cd
lccui <15720609920@163.com>
2019-11-03 16:14:06 +0800

    springboot学习

---
+++ Chapter2/src/main/java/hello/SampleController.java
@@ -0,0 +1,20 @@
+package hello;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleController.class,args);
+    }
+}
